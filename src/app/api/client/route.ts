f69a33ff016073269b0c0879793088badf6f5e68
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-12-14 21:29:40 -0300

    feat: create function post and get prisma

---
+++ src/app/api/client/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const client = await prisma.client.findMany();
+
+  return NextResponse.json({ client });
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const { name, cpf, place } = body;
+
+  if (!name) {
+    return NextResponse.json({ message: "Name is empty" });
+  }
+
+  const client = await prisma.client.create({
+    data: {
+      name,
+      cpf,
+      place,
+    },
+  });
+
+  return NextResponse.json({ client });
+}
