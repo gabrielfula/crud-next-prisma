8793ebdd7349f0581a0cd266179e6224613a223f
gabrielfula <gabriel.fulaneto0301@gmail.com>
2023-12-26 22:45:47 -0300

    feat: create component client

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,7 @@
 import Client from "@/components/client";
 import CreateUser from "@/components/create";
 
-import Header from "@/components/header";
-import {
-  Table,
-  TableCaption,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import prisma from "@/lib/prisma";
-
-export default async function Home() {
-  const data = await prisma.client.findMany({});
-
+export default function Home() {
   return (
     <div>
       <div className="m-20">
@@ -25,37 +13,7 @@ export default async function Home() {
             <h2 className="uppercase text-blue-700 font-bold">Dashboard</h2>
           </div>
         </div>
-        <Table>
-          <TableCaption>
-            {data.length < 1 ? (
-              <p>No Clients Registered at the moment.</p>
-            ) : (
-              <p>A list of your recents clients.</p>
-            )}
-          </TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[100px]">Actions</TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Place</TableHead>
-              <TableHead>CPF</TableHead>
-              <TableHead className="text-right">Date Created</TableHead>
-            </TableRow>
-          </TableHeader>
-          {data.map((item) => {
-            return (
-              <Client
-                id={item.id}
-                name={item.name}
-                status={item.status}
-                place={item.place}
-                cpf={item.cpf}
-                createdAt={item.createdAt.toISOString()}
-              />
-            );
-          })}
-        </Table>
+        <Client />
       </div>
     </div>
   );
