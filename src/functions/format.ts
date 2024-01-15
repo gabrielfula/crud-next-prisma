bc017db2c0ab99b5f7d9018e272e636bf7bcdcf8
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-01-14 22:08:25 -0300

    refactor: function format

--- src/functions/format.ts
+++ src/functions/format.ts
@@ -4,7 +4,6 @@ export const dateFormatter = (date: Date, locale = "pt-BR") => {
   return new Intl.DateTimeFormat(locale).format(date);
 };
 
-
 export const createClientSchema = z.object({
   name: z.string().nonempty("Nome é obrigatório"),
   place: z.string().nonempty("Endereço é obrigatório"),
--- src/utils/format.ts
+++
@@ -1,3 +0,0 @@
-export const dateFormatter = (date: Date, locale = "pt-BR") => {
-  return new Intl.DateTimeFormat(locale).format(date);
-};
