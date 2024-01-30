5abef847ddd1b45cce5ba79236dd9148ca65e690
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-01-30 18:07:35 -0300

    fix: fix cached prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var cachedPrisma: PrismaClient;
+}
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient();
   }
-  prisma = global.prisma;
+  prisma = global.cachedPrisma;
 }
 
 export default prisma;
