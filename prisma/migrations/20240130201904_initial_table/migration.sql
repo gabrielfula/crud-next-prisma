dd1ada97c01e82cd86217a3cceacf211eda5de56
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-01-30 18:08:20 -0300

    chore: install and first setup prisma

---
+++ prisma/migrations/20240130201904_initial_table/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "clients" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "cpf" VARCHAR(11) NOT NULL,
+    "address" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "complement" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "clients_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "clients_cpf_key" ON "clients"("cpf");
