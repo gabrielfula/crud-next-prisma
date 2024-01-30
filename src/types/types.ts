16106b3318998b95ca2517bf70bc8edcc19a00b0
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-01-30 18:13:56 -0300

    fix: delete props clients

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,16 +1,7 @@
-export type ClientesProps = {
-  id: string;
-  name: string;
-  place: string;
-  cpf: string;
-  status?: boolean;
-  createdAt: Date;
-  updatedAt?: Date;
-};
-
-
 export type FormType = {
   name: string;
   cpf: string;
-  place: string;
+  address: string;
+  price: number;
+  complement: string;
 };
