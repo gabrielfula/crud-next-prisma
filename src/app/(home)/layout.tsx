5bbfa44d7080f4e2b4b6561f39262f7f199d688d
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-01 08:48:17 -0300

    style: change ui toaster

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 import { AuthProvider } from "@/providers/auth";
 import Navbar from "@/components/header";
-import { Toaster } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
