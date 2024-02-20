891dce8a972d943da487544e3f5b7612a394dc8f
gabrielfula <gabriel.fulaneto0301@gmail.com>
2024-02-20 13:43:57 -0300

    feat: create a imaginary number

--- src/components/boleto.tsx
+++ src/components/boleto.tsx
@@ -13,10 +13,7 @@ export default function Boleto() {
           <div className="flex items-center p-2 border rounded w-full">
             <div className="flex items-center">
               <p className="max-w-[430px] whitespace-nowrap overflow-hidden text-ellipsis">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis
-                nemo autem incidunt. Quasi ratione earum, possimus doloremque
-                maiores assumenda cumque. Quos tenetur eveniet in autem iusto
-                nam eum et beatae.
+                00190500954014481606906809350314337370000010131.
               </p>
               <button className="border border-zinc-400 p-1">
                 <Copy size={20} strokeWidth={1.25} />
